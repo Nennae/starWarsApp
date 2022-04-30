1b1ef92db814a17aaabcab3de93c2a018d8f7a4c
Nennae <sezenerdal@outlook.com>
2022-04-30 12:49:42 +0200

    added images to async function

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ import { getCharacters } from "../js/swapi.js";
 console.log(Characters);
 console.log(getCharacters);
 
+      const submitBtn = document.querySelector("#submitBtn");
+      const resetBtn = document.querySelector("#resetBtn");
+      const mainContent = document.getElementById("main");
+
       function createCharCard (character) {
 
             let image = document.createElement("img");
@@ -15,19 +19,19 @@ console.log(getCharacters);
             characterCard.classList.add("characterCard");
             characterCard.innerHTML= `<h2>Hey! I'm <span class="nameStyle">${character.name}.</span></h2>`;
 
-            let genderBtn = document.createElement("button");
+            const genderBtn = document.createElement("button");
             genderBtn.classList.add("genderBtn");
             genderBtn.innerText = "Compare Gender";
 
-            let weightBtn = document.createElement("button");
+            const weightBtn = document.createElement("button");
             weightBtn.classList.add("weightBtn");
             weightBtn.innerText = "Compare Weight";
 
-            let heightBtn = document.createElement("button");
+            const heightBtn = document.createElement("button");
             heightBtn.classList.add("heightBtn");
             heightBtn.innerText = "Compare Height";
 
-            let haircolorBtn = document.createElement("button");
+            const haircolorBtn = document.createElement("button");
             haircolorBtn.innerText = "haircolorBtn";
             haircolorBtn.innerText = "Compare Haircolor";
 
@@ -40,14 +44,10 @@ console.log(getCharacters);
             mainContent.appendChild(characterCard);
       }
 
-      const submitBtn = document.querySelector("#submitBtn");
-      const resetBtn = document.querySelector("#resetBtn");
-      const mainContent = document.getElementById("main");
-      
+      createCharCard();
+
       submitBtn.addEventListener("click", () => {
-            createCharCard();
-            let charContainer1 = document.querySelector("#character1");
-            let charContainer2 = document.querySelector("#character2");
+            
             let select1 = document.querySelector("#select1").value;
             let select2 = document.querySelector("#select2").value;
             if(select1 !== select2) {
@@ -59,6 +59,63 @@ console.log(getCharacters);
                   console.log("Please choose two different characters!")
             }
       })
+
+      function compareCharacters (character, otherChar) {
+
+            let charContainer = document.querySelector(".character");
+
+            weightBtn.addEventListener("click", (e) => {
+                  let weightDiff = character.compareWeight(otherChar);
+                  e.preventDefault();
+                  if (weightDiff > 0){
+                        charContainer.innerHTML = `<li>${character.name} weighs ${character.mass} and is ${weightDiff} heavier than ${otherChar.name}
+                        </li>`
+                  } else if (weightDiff < 0) {
+                        charContainer.innerHTML = `<li>${character.name} weighs ${character.mass} and is ${weightDiff} lighter than ${otherChar.name}</li>`
+                  } else {
+                        charContainer.innerHTML = `<li>${character.name} weighs ${character.mass} and so do ${otherChar.name}</li>`
+                  }
+            })
+
+            heightBtn.addEventListener("click", (e) => {
+                  let heightDiff = character.compareHeight(otherChar);
+                  e.preventDefault();
+                  if (heightDiff > 0){
+                        charContainer.innerHTML = `<li>${character.name} is ${character.height} tall, and is ${heightDiff} taller than ${otherChar.name}
+                        </li>`
+                  } else if (weightDiff < 0) {
+                        charContainer.innerHTML = `<li>${character.name} is ${character.height} tall, and is ${heightDiff} shorter than ${otherChar.name}</li>`
+                  } else {
+                        charContainer.innerHTML = `<li>${character.name} is ${character.height} tall, and so is ${otherChar.name}</li>`
+                  }
+            })
+
+            hairColroBtn.addEventListener("click", (e) => {
+                  let hairColor = character.compareHairColor(otherChar);
+                  e.preventDefault();
+                  if (hairColor){
+                        charContainer.innerHTML = `<li>${character.name} and     ${otherChar.name} has the same hair color which is ${character.hairColor}
+                        </li>`
+                  } else {
+                        charContainer.innerHTML = `<li>${character.name}s hair color is ${character.hairColor} and ${otherChar.name}s is ${otherChar.hairColor}
+                        </li>`
+                  }
+            })
+
+            genderBtn.addEventListener("click", (e) => {
+                  let gender = character.compareGender(otherChar);
+                  e.preventDefault();
+                  if (gender){
+                        charContainer.innerHTML = `<li>${character.name} and     ${otherChar.name} has the same gender which is ${character.gender}
+                        </li>`
+                  } else {
+                        charContainer.innerHTML = `<li>${character.name}s gender is ${character.gender} and ${otherChar.name}s is ${otherChar.gender}
+                        </li>`
+                  }
+            })
+      }
+
+      compareCharacters()
 
 resetBtn.addEventListener("click", () => {
       location.reload();
--- js/swapi.js
+++ js/swapi.js
@@ -1,14 +1,27 @@
 import { Characters } from "../js/characters.js";
 
 export let getCharacters = async (names) => {
+
+      let charImgs = {
+            "Luke Skywalker": "./images/luke_skywalker.jpeg",
+            "Obiwan Kenobi": "./images/obiwan_kenobi.jpg",
+            "Leia Organa": "./images/leiaiv.jpeg",
+            "Darth Vader": "./images/darth-vader-cool.jpeg",
+            "Chewbacca": "./images/chewierawr.jpeg",
+            "Biggs Darklighter": "./images/Biggs.jpg",
+            "R5-D4":"./images/R5-D4.jpeg",
+            "C-3PO": "./images/C-3PO.jpeg"
+      };
+
       let response = await fetch(`https://swapi.dev/api/people/?search=${names}`);
       let characterInfo = await response.json();
+      console.log(characterInfo.results[0].name)
       return new Characters ( 
             characterInfo.results[0].name,
             characterInfo.results[0].gender,
             parseInt(characterInfo.results[0].height),
             parseInt(characterInfo.results[0].mass),
             characterInfo.results[0].hairColor,
-            pictureURL[characterInfo.results[0].name]
+            charImgs[characterInfo.results[0].name]
       );
-};
+};    
